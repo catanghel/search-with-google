11715009edf27276ea792670d4eaff96c0f7021f
Catalin Anghel <catalin.anghel@live.com>
2014-06-16 12:13:04 +0300

    Added Stackoverflow

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/*global define, brackets, window */
 
-/** Simple extension that adds a "File > Search with Google" menu item */
 define(function (require, exports, module) {
     "use strict";
 
@@ -14,14 +13,25 @@ define(function (require, exports, module) {
 		var query = thisEditor._codeMirror.getSelection();
         window.open("https://www.google.com/#q=" + encodeURIComponent(query));
     }
+	
+	function searchOnStackOverflow() {
+		var thisEditor = EditorManager.getCurrentFullEditor();
+		var query = thisEditor._codeMirror.getSelection();
+        window.open("https://www.stackoverflow.com/search?q=" + encodeURIComponent(query));
+    }
 
-    var MY_COMMAND_ID = "catanghel.searchwithgoogle"; 
-    CommandManager.register("Search with Google", MY_COMMAND_ID, searchWithGoogle);
+    var GOOGLE_SEARCH_COMMAND = "catanghel.searchwithgoogle"; 
+	var STACKOVERFLOW_SEARCH_COMMAND = "catanghel.searchonstackoverflow";
+	
+    CommandManager.register("Search with Google", GOOGLE_SEARCH_COMMAND, searchWithGoogle);
+	CommandManager.register("Search on Stackoverflow", STACKOVERFLOW_SEARCH_COMMAND, searchOnStackOverflow);
 
     var fileMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
 	var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
 	fileMenu.addMenuDivider();
-    fileMenu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-G");
+    fileMenu.addMenuItem(GOOGLE_SEARCH_COMMAND, "Ctrl-Alt-G");
+	fileMenu.addMenuItem(STACKOVERFLOW_SEARCH_COMMAND, "Ctrl-Alt-W");
 	contextMenu.addMenuDivider();
-	contextMenu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-G");
+	contextMenu.addMenuItem(GOOGLE_SEARCH_COMMAND, "Ctrl-Alt-G");
+	contextMenu.addMenuItem(STACKOVERFLOW_SEARCH_COMMAND, "Ctrl-Alt-W");
 });
